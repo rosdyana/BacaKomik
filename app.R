d4bc4bb0762f206770822fcfc204923ed8403d78
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-05-03 10:22:41 +0800

    Update app.R

--- app.R
+++ app.R
@@ -3,52 +3,85 @@ library(XML)
 library(RCurl)
 
 server <- function(input, output) {
-  output$view <- renderUI({
-    data = list()
-    i = 1
-    # http://images.mangafreak.net/mangas/one_piece/one_piece_863/one_piece_863_19.jpg
-    urlx = paste(
-      "http://images.mangafreak.net/mangas/one_piece/one_piece_",
-      input$n,
-      "/one_piece_",
-      input$n,
-      "_",
-      sep = ""
-    )
-    for (p in 1:30)
+  cid <- reactive({
+    switch(input$comic,
+           "海賊王" = 2,
+           "獵人" = 10,
+           "火影忍者" = 1)
+  })
+  
+  URLInput <- reactive({
+    url=""
+    if(input$type == "Volume")
     {
-      url = paste(urlx, p, ".jpg", sep = "")
-      if (!url.exists(url))
+      if (input$n<10)
       {
-        # break
-        # due to server restriction is better not check the exist url :sad
+        url=paste("http://manhua.fzdm.com/",cid(),"/Vol_00",input$n,sep="")
       }
-      url.list = url
-      data[[i]] = tags$img(src = url.list,
-                           width = 600,
-                           heigth = 600)
-      i = i + 1
-      data[[i]] = br()
-      i = i + 1
+      else
+      {
+        url=paste("http://manhua.fzdm.com/",cid(),"/Vol_0",input$n,sep="")
+      }
     }
-    data
+    else
+    {
+      url=paste("http://manhua.fzdm.com/",cid(),"/",input$n,sep="")
+    }
+    url
+  })
+  
+  output$view <- renderUI({
+    urlx=URLInput()
+    urlt =paste(urlx,"/index_0.html",sep="")
+    if (!url.exists(urlt))
+    {
+      h1(paste("Error : Please check ",input$type," number."))
+    }
+    else
+    {
+      data = list()
+      range=120
+      i=1
+      if (input$type=="Chapter")
+      {
+        range=25
+      }
+      for (p in 0:range)
+      {
+        url =paste(urlx,"/index_",p,".html",sep="")
+        if (!url.exists(url))
+        {
+          break
+        }
+        html = htmlParse(getURL(url,ssl.verifypeer = FALSE))
+        url.list = xpathSApply(html, "//li/a/img[@src]", xmlAttrs)
+        data[[i]]=tags$img(src=url.list[1],width=500,heigth=500)
+        i=i+1
+        data[[i]]=br()
+        i=i+1
+      }
+      data
+    }
   })
 }
 
-ui <- fluidPage(titlePanel("Baca Komik - One Piece"),
-                sidebarLayout(
-                  sidebarPanel(
-                    sliderInput(
-                      "n",
-                      "Chapter:",
-                      min = 700,
-                      max = 863,
-                      value = 853
-                    ),
-                    submitButton("Get Comics")
-                    
-                  ),
-                  mainPanel(htmlOutput("view"))
-                ))
+ui <- fluidPage(
+  titlePanel("Comic Viewer"),  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("comic", "Choose a Comic:", 
+                  choices = c("海賊王", "獵人", "火影忍者")),      
+      
+      helpText("Ref : http://manhua.fzdm.com/"),
+      radioButtons("type", "Select Type:", choices = c( "Volume","Chapter"),"Chapter"),
+      sliderInput("n", "Number:", min=1, max=1000, value=828),
+      submitButton("Get Comics")
+      
+    ),    
+    mainPanel(     
+      htmlOutput("view")
+    )
+  )
+)
 
 shinyApp(ui = ui, server = server)
